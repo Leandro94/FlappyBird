e0c120143216670c385f18d52a15620b65e0446f
LeandroR94 <reisleandro868@gmail.com>
2019-07-01 10:25:47 -0300

    Criação do projeto.

---
+++ android/src/com/leandro/flappybird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.leandro.flappybird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.leandro.flappybird.Jogo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Jogo(), config);
+	}
+}
